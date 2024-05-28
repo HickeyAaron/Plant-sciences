187f26d874e8500eee033b4f3c0662e921012daa
HickeyAaron <115097595+HickeyAaron@users.noreply.github.com>
2024-05-28 18:06:45 +0100

    Create csv_concatenation.R

---
+++ stomatal-opening-automation/csv_concatenation.R
@@ -0,0 +1,37 @@
+library(dplyr)
+
+# setwd("/Users/aaronhickey/Ilastik/test_output/")
+
+# Set the directory where your CSV files are located
+directory <- "/path/to/csv/files"
+
+
+# List all CSV files in the directory
+csv_files <- list.files(directory, pattern = "\\.csv$", full.names = TRUE)
+
+
+# Function to read and modify each CSV file
+process_csv <- function(file) {
+  # Read the CSV file
+  data <- read.csv(file, header = TRUE)
+  
+  # Extract file name without extension
+  file_name <- tools::file_path_sans_ext(basename(file))
+  
+  # Add a new column 'Sample' with the file name
+  data$Image <- file_name
+  
+  # Return the modified data
+  return(data)
+}
+
+# Apply the function to each CSV file
+processed_data <- lapply(csv_files, process_csv)
+
+# Concatenate all processed data frames into one
+combined_data <- do.call(rbind, processed_data)
+
+head(Clean_data)
+
+# Write the cleaned data to a single CSV file
+write.csv(Clean_data, file = "combined_samples.csv", row.names = FALSE)
